3c95f596b664e9a403cb0e34829ba75d99c44be8
Donato <donato.gr@gmail.com>
2020-05-03 12:47:03 +0200

    Update views.py
    Fix TypeError: unsupported operand type(s) for +: 'dict_items' and 'dict_items'

--- admin_export/views.py
+++ admin_export/views.py
@@ -117,5 +117,5 @@ class AdminExportRelated(GetFieldsMixin, TemplateView):
         context['model_ct'] = model_ct.id
         context['field_name'] = field_name
         context['table'] = True
-        context = dict(context.items() + field_data.items())
+        context = dict(list(context.items()) + list(field_data.items()))
         return self.render_to_response(context)
